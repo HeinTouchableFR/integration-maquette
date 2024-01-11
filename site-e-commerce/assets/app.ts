c6e2442f719831320a02530c93a4b07c7d031f51
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2024-01-11 21:06:00 +0100

    Produits et catégories

--- site-e-commerce/assets/app.ts
+++ site-e-commerce/assets/app.ts
@@ -2,7 +2,12 @@ import "/styles/app.scss";
 
 import { registerSlider } from "/modules/Slider";
 import { registerCarousel } from "/modules/Carousel";
+import {Products} from "/modules/Products";
 
 
 registerSlider();
 registerCarousel();
+
+const products = new Products();
+products.buildCard("categories")
+products.buildCard("products")
